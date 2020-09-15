86528554a49609621ed47f4c53f252ace0a4aca1
mark2447 <hwj97055@gmail.com>
2020-09-15 17:29:15 +0800

    GameBoard.java modification

--- hw1/src/main/java/models/GameBoard.java
+++ hw1/src/main/java/models/GameBoard.java
@@ -15,5 +15,33 @@ public class GameBoard {
   private int winner;
 
   private boolean isDraw;
-
+  
+  //empty constructor
+  public GameBoard() {
+	  this.p1 = null;
+	  this.p2 = null;
+	  this.gameStarted = false;
+	  this.turn = 1;
+	  this.boardState = new char[3][3];
+	  this.winner = 1;
+	  this.isDraw = false;
+  }
+  
+  public void setPlayer1(Player p1) {
+	  this.p1 = p1;
+  }
+  
+  public Player getPlayer1() {
+	  return this.p1;
+  }
+  
+  public void setPlayer2(Player p2) {
+	  this.p2 = p2;
+  }
+  
+  public Player getPlayer2() {
+	  return this.p2;
+  }
+  
+  
 }
